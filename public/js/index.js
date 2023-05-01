b1b513890a84fcdb9a6e7d3fe9f4eda60d4fd01c
a_abdirahim <cabtamido@gmail.com>
2023-05-02 01:12:14 +0300

    Intergrating chart.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,139 +1,176 @@
-const ctx = document.getElementById('myChart').getContext('2d');
-const ctx2 = document.getElementById('myChart2').getContext('2d');
-const ctx3 = document.getElementById('myChart3').getContext('2d');
+const ctx = document.getElementById("myChart").getContext("2d");
+const ctx2 = document.getElementById("myChart2").getContext("2d");
+const ctx3 = document.getElementById("myChart3").getContext("2d");
 
-const getTopLanguagesAndRepo = async () => {
+const getRepoData = async () => {
   const url = `https://api.github.com/users/jonasschmedtmann/repos`;
-
   try {
     const response = await fetch(url);
-    const repos = await response.json();
+    const repositories = await response.json();
 
-    // Get top languages
-    const languageCounts = {};
-    await Promise.all(
-      repos.map(async (repo) => {
-        const languagesUrl = `${repo.url}/languages`;
-        const languagesResponse = await fetch(languagesUrl);
-        const languages = await languagesResponse.json();
-        Object.keys(languages).forEach((language) => {
-          languageCounts[language] = (languageCounts[language] || 0) + languages[language];
-        });
-      })
-    );
-    const topLanguages = Object.entries(languageCounts)
-      .sort((a, b) => b[1] - a[1])
-      .slice(0, 3)
-      .reduce((obj, [key, value]) => {
-        obj[key] = value;
-        return obj;
-      }, {});
-
-    // Get most starred repo
-    const mostStarredRepo = repos.reduce((a, b) => (a.stargazers_count > b.stargazers_count ? a : b));
-
-    return { topLanguages, mostStarredRepo };
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-};
-
-
-const generateChart = async () => {
-  const generateChart = async () => {
-    const data = await getTopLanguagesAndRepo();
-    const backgroundColorss = [
-      '#f5e98b', // Bar 1
-      '#8877a3', // Bar 2
-      '#838db5', // Bar 3
-    ];
-    const labels = Object.keys(data.topLanguages).concat(data.mostStarredRepo.name);
-    const dataValues = Object.values(data.topLanguages).concat(data.mostStarredRepo.stargazers_count);
-    const backgroundColors = backgroundColorss.slice(0, 3).concat(data.mostStarredRepo.language);
-  
-    const config = {
-      type: 'doughnut',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Number of GitHub Stars',
-          data: dataValues,
-          backgroundColor: backgroundColors,
-          borderWidth: 2,
-          borderColor: 'black'
-        }]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: true,
-        plugins: {
-          legend: {
-            position: 'top',
-          },
-          title: {
-            display: true,
-            text: 'Chart.js Doughnut Chart'
-          }
+    const languagesCount = {};
+    repositories.forEach((repo) => {
+      const language = repo.language;
+      if (language) {
+        if (languagesCount[language]) {
+          languagesCount[language]++;
+        } else {
+          languagesCount[language] = 1;
         }
       }
-    };
-  
-    const myChart = new Chart(ctx, config);
-  };
-  
+    });
 
+    const sortedLanguages = Object.entries(languagesCount)
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, 5);
 
-  const myChart3 = new Chart(ctx3, {
-    type: "pie",
-    data: {
-      labels: Object.keys(data.topLanguages).concat(data.mostStarredRepo.name),
-      datasets: [
-        {
-          label: "Number of GitHub Stars",
-          data: Object.values(data.topLanguages).concat(data.mostStarredRepo.stargazers_count),
-          backgroundColor: backgroundColors.slice(0, 3).concat(data.mostStarredRepo.language),
-          borderWidth: 2,
-          borderColor: "black",
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      // maintainAspectRatio: false,
-      plugins: {
-        title: {
-            display: true,
-            text: 'Custom Chart Title'
-        }
-    }
-    },
-  });
-  const myChart2 = new Chart(ctx2, {
-    type: "bar",
-    data: {
-      labels: Object.keys(data.topLanguages).concat(data.mostStarredRepo.name),
-      datasets: [
-        {
-          label: "Number of GitHub Stars",
-          data: Object.values(data.topLanguages).concat(data.mostStarredRepo.stargazers_count),
-          backgroundColor: backgroundColors.slice(0, 3).concat(data.mostStarredRepo.language),
-          borderWidth: 2,
-          borderColor: "black",
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      // maintainAspectRatio: false,
-       plugins: {
-            title: {
-                display: true,
-                text: 'Custom Chart Title'
-            }
-        }
-    },
-  });
+    const labels = sortedLanguages.map((language) => language[0]);
+    const data = sortedLanguages.map((language) => language[1]);
+
+    return { labels, data };
+  } catch (err) {}
 };
 
-generateChart();
+const getTopStarred = async () => {
+  const response = await fetch(`https://api.github.com/users/jonasschmedtmann/repos`);
+    const repositories = await response.json();
+  
+    // Iterate through repositories and count stars per language
+    const starsPerLanguage = {};
+    for (let repo of repositories) {
+      // Fetch languages for repository
+      const languagesResponse = await fetch(repo.languages_url);
+      const languagesData = await languagesResponse.json();
+      
+      // Iterate through languages and add stars count for each language
+      for (let language in languagesData) {
+        if (starsPerLanguage[language]) {
+          starsPerLanguage[language] += languagesData[language];
+        } else {
+          starsPerLanguage[language] = languagesData[language];
+        }
+      }
+    }
+  
+    // Convert stars per language object into an array
+    const data = [];
+    for (let language in starsPerLanguage) {
+      data.push({
+        language: language,
+        stars: starsPerLanguage[language]
+      });
+    }
+  
+    // Sort data by stars count in descending order
+    data.sort((a, b) => b.stars - a.stars);
+  
+    // Return top 5 languages by stars count
+    return data.slice(0, 5);
+};
+
+const getMostStarred = async () => {
+   // Fetch repos data from API
+   const response = await fetch(`https://api.github.com/users/jonasschmedtmann/repos?per_page=1000`);
+   const data = await response.json();
+
+   // Sort repos by stargazers count in descending order
+   data.sort((a, b) => b.stargazers_count - a.stargazers_count);
+
+   // Get the top 5 most starred repos
+   const topRepos = data.slice(0, 5);
+
+   // Extract repo names and stargazers count
+   const repoNames = topRepos.map(repo => repo.name);
+   const stargazersCount = topRepos.map(repo => repo.stargazers_count);
+   return {repoNames, stargazersCount}
+};
+
+const renderChart = async()=>{
+  const { labels, data } = await getRepoData();
+  new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Number of Votes",
+          data: data,
+          backgroundColor: ['#ff91a9',
+        '#72bef1',
+        '#ffdd88',
+        '#81d3d3',
+        '#8877a3'
+      ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          position: 'right',
+        },
+      }
+    },
+  });
+  const {repoNames, stargazersCount}= await getMostStarred();
+  new Chart(ctx2, {
+    type: "bar",
+    data: {
+      labels: repoNames,
+      datasets: [{
+        // label: "hello",
+        data: stargazersCount,
+        backgroundColor: ['#ff91a9',
+        '#72bef1',
+        '#ffdd88',
+        '#81d3d3',
+        '#8877a3']
+      }]
+    },
+    options: {
+      responsive: false,
+      plugins: {
+        legend: {
+          display: false
+        },
+      },
+      maintainAspectRatio: false
+    }
+  });
+  // top starred language chart
+  const topstarsLanguages = await getTopStarred();
+  // Extract labels and data values for Chart.js
+  const labelss = topstarsLanguages.map(datum => datum.language);
+  const values = topstarsLanguages.map(datum => datum.stars);
+
+  // Create a new Chart.js doughnut chart
+  new Chart(ctx3, {
+    type: 'pie',
+    data: {
+      labels: labelss,
+      datasets: [{
+        label: 'Stars per Language',
+        data: values,
+        backgroundColor: [
+          '#ff91a9',
+          '#72bef1',
+          '#ffdd88',
+          '#81d3d3',
+          '#8877a3'
+        ],
+      }]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'right',
+        },
+      },
+      // maintainAspectRatio: false
+    }
+  });  
+}
+
+renderChart()
